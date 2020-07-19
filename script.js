880aacbada77d00632898240f6545e2c88b863cf
MarnixCools <marnixcools2@msn.com>
2020-07-19 14:54:24 +0200

    Adding first files

---
+++ script.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8,9];
+
+console.log('answer: ', _.without(array,5));
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+function SetGradient(){
+	body.style.background = 
+	  "linear-gradient(to right, "
+	  + color1.value 
+	  + ", " 
+	  + color2.value 
+	  + " )";	
+
+	  css.textContent = body.style.background ;
+}
+
+color1.addEventListener("input", SetGradient ); 
+
+color2.addEventListener("input", SetGradient ); 
+
